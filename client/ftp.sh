37b781e1e1adcbe5c17c62f9a0700370c18eeb97
KevinDelcourt <delcokevgmail.com>
2019-02-19 00:14:22 +0100

    fix: travis

--- client/ftp.sh
+++ client/ftp.sh
@@ -1,1 +1,1 @@
-find * -type f -exec curl -u ${FTP_USER}\@kevin-delcourt.net:${FTP_PSWD} --ftp-create-dirs -T {} ftp.online.net/soundbox/{} \;
+find /client/build/* -type f -exec curl -u ${FTP_USER}\@kevin-delcourt.net:${FTP_PSWD} --ftp-create-dirs -T {} ftp.online.net/soundbox/{} \;
