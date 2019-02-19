56abc8b92d1ad27a8016672f6314d21ee2490bf0
KevinDelcourt <delcokevgmail.com>
2019-02-19 09:25:45 +0100

    fix: travis

--- client/ftp.sh
+++ client/ftp.sh
@@ -1,21 +1,20 @@
-cd ./build;
 #find * -type f -exec curl -u ${FTP_USER}\@kevin-delcourt.net:${FTP_PSWD} --ftp-create-dirs -T {} ftp.online.net/soundbox/{} --no-epsv \;
 for entry in *.*
 do
   echo $entry
-  curl -T $entry -u ${FTP_USER}\@kevin-delcourt.net:${FTP_PSWD} --ftp-create-dirs  ftp.online.net/soundbox/$entry --no-epsv -s \;
+  curl -T $entry -u ${FTP_USER}\@kevin-delcourt.net:${FTP_PSWD} --ftp-create-dirs  ftp.online.net/soundbox/$entry -S \;
 done
 
 cd ./static/js;
 for entry in *.*
 do
   echo $entry
-  curl -T $entry -u ${FTP_USER}\@kevin-delcourt.net:${FTP_PSWD} --ftp-create-dirs  ftp.online.net/soundbox/static/js/$entry --no-epsv -s \;
+  curl -T $entry -u ${FTP_USER}\@kevin-delcourt.net:${FTP_PSWD} --ftp-create-dirs  ftp.online.net/soundbox/static/js/$entry -S \;
 done
 
 cd ./../media;
 for entry in *.*
 do
   echo $entry
-  curl -T $entry -u ${FTP_USER}\@kevin-delcourt.net:${FTP_PSWD} --ftp-create-dirs  ftp.online.net/soundbox/static/media/$entry --no-epsv -s \;
+  curl -T $entry -u ${FTP_USER}\@kevin-delcourt.net:${FTP_PSWD} --ftp-create-dirs  ftp.online.net/soundbox/static/media/$entry -S \;
 done
