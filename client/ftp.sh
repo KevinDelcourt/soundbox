6f90240ae92ae05efa18802e37f31b859afc968a
KevinDelcourt <delcokevgmail.com>
2019-02-19 08:34:40 +0100

    fix: travis

--- client/ftp.sh
+++ client/ftp.sh
@@ -1,2 +1,21 @@
-cd build;
-find * -type f -exec curl -u ${FTP_USER}\@kevin-delcourt.net:${FTP_PSWD} --ftp-create-dirs -T {} ftp.online.net/soundbox/{} --no-epsv \;
+cd ./build;
+#find * -type f -exec curl -u ${FTP_USER}\@kevin-delcourt.net:${FTP_PSWD} --ftp-create-dirs -T {} ftp.online.net/soundbox/{} --no-epsv \;
+for entry in *.*
+do
+  echo $entry
+  curl -T $entry -u ${FTP_USER}\@kevin-delcourt.net:${FTP_PSWD} --ftp-create-dirs  ftp.online.net/soundbox/$entry --no-epsv \;
+done
+
+cd ./static/js;
+for entry in *.*
+do
+  echo $entry
+  curl -T $entry -u ${FTP_USER}\@kevin-delcourt.net:${FTP_PSWD} --ftp-create-dirs  ftp.online.net/soundbox/static/js/$entry --no-epsv \;
+done
+
+cd ./../media;
+for entry in *.*
+do
+  echo $entry
+  curl -T $entry -u ${FTP_USER}\@kevin-delcourt.net:${FTP_PSWD} --ftp-create-dirs  ftp.online.net/soundbox/static/media/$entry --no-epsv \;
+done
