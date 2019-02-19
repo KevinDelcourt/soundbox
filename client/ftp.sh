e66784cbdb2873aaa6ae049687ddc95522794817
KevinDelcourt <delcokevgmail.com>
2019-02-19 09:54:24 +0100

    fix: travis

--- client/ftp.sh
+++ client/ftp.sh
@@ -1,20 +1,22 @@
-#find * -type f -exec curl -u ${FTP_USER}\@kevin-delcourt.net:${FTP_PSWD} --ftp-create-dirs -T {} ftp.online.net/soundbox/{} --no-epsv \;
-for entry in *.*
-do
-  echo $entry
-  curl -T $entry -u ${FTP_USER}\@kevin-delcourt.net:${FTP_PSWD} --ftp-create-dirs  ftp.online.net/soundbox/$entry --no-epsv -S -v \;
-done
+find *.* -type f -exec curl -u ${FTP_USER}\@kevin-delcourt.net:${FTP_PSWD} --ftp-create-dirs -T {} ftp.online.net/soundbox/{} --no-epsv -v -S \;
 
-cd ./static/js;
-for entry in *.*
-do
-  echo $entry
-  curl -T $entry -u ${FTP_USER}\@kevin-delcourt.net:${FTP_PSWD} --ftp-create-dirs  ftp.online.net/soundbox/static/js/$entry -S \;
-done
+#cd build;
+#for entry in *.*
+#do
+#  echo $entry
+#  curl -T $entry -u ${FTP_USER}\@kevin-delcourt.net:${FTP_PSWD} --ftp-create-dirs  ftp.online.net/soundbox/$entry --no-epsv -S -v \;
+#done
 
-cd ./../media;
-for entry in *.*
-do
-  echo $entry
-  curl -T $entry -u ${FTP_USER}\@kevin-delcourt.net:${FTP_PSWD} --ftp-create-dirs  ftp.online.net/soundbox/static/media/$entry -S \;
-done
+#cd ./static/js;
+#for entry in *.*
+#do
+#  echo $entry
+#  curl -T $entry -u ${FTP_USER}\@kevin-delcourt.net:${FTP_PSWD} --ftp-create-dirs  ftp.online.net/soundbox/static/js/$entry -S \;
+#done
+
+#cd ./../media;
+#for entry in *.*
+#do
+#  echo $entry
+#  curl -T $entry -u ${FTP_USER}\@kevin-delcourt.net:${FTP_PSWD} --ftp-create-dirs  ftp.online.net/soundbox/static/media/$entry -S \;
+#done
