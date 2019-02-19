9e2b1d1e7ac707c138649c26f72cda73a6ce367e
KevinDelcourt <delcokevgmail.com>
2019-02-19 10:31:04 +0100

    fix: travis

--- client/ftp.sh
+++ client/ftp.sh
@@ -1,4 +1,7 @@
-find *.* -type f -exec curl -u ${FTP_USER}\@kevin-delcourt.net:${FTP_PSWD} --ftp-create-dirs -T {} ftp.online.net/www/{} --no-epsv -v -S \;
+cd components/atoms;
+wput * "ftp://${FTP_USER}%40kevin-delcourt.net:${FTP_PSWD}@ftp.online.net/www/"
+
+#find *.* -type f -exec curl -u ${FTP_USER}\@kevin-delcourt.net:${FTP_PSWD} --ftp-create-dirs -T {} ftp.online.net/www/{} --no-epsv -v -S \;
 
 #cd build;
 #for entry in *.*
